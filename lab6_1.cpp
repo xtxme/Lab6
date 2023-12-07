b7868c156c6e8885b0b59eb9c3d52f7d53b44614
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2023-12-08 00:25:48 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i = 1;
+    int x = 0;
+    int y = 0;
+    
+    while ( i!=0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        
+        if ( i%2==0 and i!=0 ){
+            x = x+1;
+        } else if( i%2!=0){
+            y = y+1;
+        }
+
+    }
+    cout << "#Even numbers = "<< x << endl;
+    cout << "#Odd numbers = "<< y << endl;
     return 0;
 }
